2a0defa22f2750376e55003b1aed92da6e35893b
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-17 20:30:36 -0300

    added search bar

--- src/screens/Projects.tsx
+++ src/screens/Projects.tsx
@@ -7,18 +7,25 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
 
 import { useProjectsContext } from "../context/ProjectsContext";
 
 import ProjectCard from "../components/ProjectCard";
+import TextField from "@mui/material/TextField";
 
 export default function Projects() {
+
+  const [search, setSearch] = React.useState<string>("")
   let navigate = useNavigate();
   function redirectAddProject(): void {
     navigate("/add-project");
   }
 
   const { projects } = useProjectsContext();
+
+
 
   return (
     <Container maxWidth="md">
@@ -37,7 +44,24 @@ export default function Projects() {
       </section>
       <Paper variant="outlined">
         <main>
-          {projects.map((project) => {
+          <TextField
+            id="search"
+            variant="outlined"
+            placeholder="Search..."
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            inputProps={{ "aria-label": "Without label" }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          {projects.filter((project)=> {
+              return project.name.includes(search)
+          }).map((project) => {
             return <ProjectCard key={project.id} project={project} />;
           })}
         </main>

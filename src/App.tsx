529b10cf5ead68fc9aaa26145dabc1d7092abb5e
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-17 14:47:05 -0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+
 import Nav from "./components/Nav"
+import AddProject from './screens/AddProject';
 import Projects from './screens/Projects';
 
 function App() {
   return (
     <div className="App">
-     <Nav />
-     <Projects />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Projects />} />
+        <Route path="/add-project" element={<AddProject />} />
+      </Routes>
     </div>
   );
 }

ec7d77fa4b6735203edf15a2764b64f0411ca8da
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-16 22:39:17 -0300

    added Projects screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
 import Nav from "./components/Nav"
+import Projects from './screens/Projects';
 
 function App() {
   return (
     <div className="App">
      <Nav />
+     <Projects />
     </div>
   );
 }

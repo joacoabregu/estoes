8563c956c6c54b6ef1c2db0d2646cbb1df111013
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-17 17:11:06 -0300

    added Context

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,31 @@ import Nav from "./components/Nav";
 import AddProject from "./screens/AddProject";
 import Projects from "./screens/Projects";
 
+import { ProjectsContext } from "./context/ProjectsContext";
+import { Project, Projects as ProjectsType } from "./types/interfaces";
+const project: Project = {
+  id: 1,
+  name: "Prueba",
+  description: "lorem ipsum",
+  projectManager: "Walt",
+  assigned: "Ignacio",
+  status: "enabled",
+  creationDate: "09/09/09 18:00 am",
+  image: "https://picsum.photos/200",
+};
+
 function App() {
+  const [projects, setProjects] = React.useState<ProjectsType>([project]);
+
   return (
     <div css={styles.main}>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Projects />} />
-        <Route path="/add-project" element={<AddProject />} />
-      </Routes>
+      <ProjectsContext.Provider value={{ projects, setProjects }}>
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/add-project" element={<AddProject />} />
+        </Routes>
+      </ProjectsContext.Provider>
     </div>
   );
 }

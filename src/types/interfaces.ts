3c1c537ef1794df053fa5ff2ff1d20dd06cc2d7e
Joaquin Abregu <abregujoaquin@gmail.com>
2022-02-17 16:54:37 -0300

    project interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -5,3 +5,21 @@ export interface FormInput {
   assignedTo: string;
   status: "enabled" | "disabled";
 }
+
+export interface Project {
+  id: number;
+  name: string;
+  description: string;
+  projectManager: string;
+  assigned: string;
+  status: string;
+  creationDate: string;
+  image: string;
+}
+
+export type Projects = Project[];
+
+export interface ProjectsContextValue {
+  state: Projects;
+  setState: () => {};
+}
